4cde7be318e61ec7644961a409a8687bce2cfcc3
Ozdemir <57063691+ozdemirmt99@users.noreply.github.com>
2024-03-05 23:28:28 +0000

    fixed home path

--- Project/Deneme/urls.py
+++ Project/Deneme/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path("",include("Home.urls")),
     path("Home/",include("Home.urls")),
     path("admin/", admin.site.urls),
 ]
