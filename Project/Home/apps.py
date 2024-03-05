1a63ff0a87151f746ca3583b44ea49f19d30681a
Ozdemir <57063691+ozdemirmt99@users.noreply.github.com>
2024-03-05 22:57:47 +0000

    planned site maps

--- Project/Home/apps.py
+++ Project/Home/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Deneme2Config(AppConfig):
+class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "deneme2"
+    name = "Home"
