18af3188740f7971c12b027e33fe6335e4ff704f
Ozdemir <57063691+ozdemirmt99@users.noreply.github.com>
2024-03-06 23:54:01 +0000

    partial views and urls index navbar

--- Project/Home/urls.py
+++ Project/Home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("",views.index),
+    path("contact/",views.contact,{"content":"contact.html"}),
+    path("products/",views.products,{"content":"products.html"}),
+    path("about/",views.about),
 ]
--- Project/Home/views.py
+++ Project/Home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"Home/index.html")
+
+def contact(request):
+    return render(request,"Home/index.html")
+
+def products(request):
+    return render(request,"Home/index.html")
+
+def about(request):
+    return render(request,"Home/_about.html")
