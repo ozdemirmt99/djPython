df314b6101c39f85100c06dfeb5968aa98813f64
Ozdemir <57063691+ozdemirmt99@users.noreply.github.com>
2024-03-09 00:02:36 +0000

    data drilling in django

--- Project/Home/urls.py
+++ Project/Home/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("",views.index,name="index"),
-    path("contact/",views.contact,name="contact"),
     path("products/",views.products,name="products"),
     path("about/",views.about,name="about"),
 ]
--- Project/Home/views.py
+++ Project/Home/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"Home/index.html",context={"content":"Home/_products.html"})
-
-def contact(request):
-    return render(request,"Home/index.html",context={"content":"Home/_contact.html"})
+    
+    return render(request,"Home/index.html",context={"content":"Home/_indexContent.html"})
 
 def products(request):
-    return render(request,"Home/index.html",context={"content":"Home/_products.html"})
+    items=[1,2,3,4]
+    return render(request,"Home/index.html",context={"content":"Home/_products.html", "items":items})
 
 def about(request):
     return render(request,"Home/index.html",context={"content":"Home/_about.html"})
